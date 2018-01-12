99158353732c63173eb3350f2580746049999693
Waevka <waveswallow@gmail.com>
2018-01-12 22:27:01 +0100

    Modified gitignore, added basic Scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float walkSpeed;
+    public float rotateSpeed;
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mov = Input.GetAxis("Vertical") * walkSpeed;
+        float rot = Input.GetAxis("Horizontal") * rotateSpeed;
+
+        rb.MovePosition(rb.position + rb.transform.forward * mov);
+        rb.MoveRotation(Quaternion.Euler(0.0f, rb.rotation.eulerAngles.y + rot, 0.0f));
+	}
+}
