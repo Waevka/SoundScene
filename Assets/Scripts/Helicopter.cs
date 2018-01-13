55261300efce7c2443d642679efa97c977d886c4
Waevka <waveswallow@gmail.com>
2018-01-13 18:48:58 +0100

    Added doppler + helicopter.

---
+++ Assets/Scripts/Helicopter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour {
+
+    [FMODUnity.EventRef]
+    public string HelicopterEvent;
+    FMOD.Studio.EventInstance HelicopterSound;
+    [SerializeField]
+    Transform helicopterModel;
+    // Use this for initialization
+    void Start () {
+        HelicopterSound = FMODUnity.RuntimeManager.CreateInstance(HelicopterEvent);
+        HelicopterSound.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(helicopterModel));
+        HelicopterSound.start();
+        print("Playing helicopter sound");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Rotate(0.0f, 0.3f, 0.0f);
+        HelicopterSound.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(helicopterModel));
+    }
+}
--- Assets/Scripts/PoliceCar.cs
+++ Assets/Scripts/PoliceCar.cs
@@ -6,10 +6,15 @@ using UnityEngine.AI;
 public class PoliceCar : MonoBehaviour {
     public Transform[] controlPoints;
     public int currentControlPoint;
+    public float dopplerPitch;
     NavMeshAgent agent;
+    [SerializeField]
+    GameObject Player;
     [FMODUnity.EventRef]
     public string PoliceEvent;
     FMOD.Studio.EventInstance PoliceAlarm;
+
+    const float SPEED_OF_SOUND = 340.0f;
     // Use this for initialization
     void Start () {
         currentControlPoint = 0;
@@ -19,7 +24,7 @@ public class PoliceCar : MonoBehaviour {
         PoliceAlarm = FMODUnity.RuntimeManager.CreateInstance(PoliceEvent);
         PoliceAlarm.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
         PoliceAlarm.start();
-        print("Playing bee sound");
+        print("Playing police car sound");
     }
 	
 	// Update is called once per frame
@@ -29,6 +34,11 @@ public class PoliceCar : MonoBehaviour {
             TravelToNextPoint();
         }
         PoliceAlarm.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        //calculate Doppler Effect
+        Vector3 directionOfListener = (transform.position - Player.transform.position).normalized;
+        float speedRelativeToListener = agent.speed * Vector3.Dot(transform.forward, directionOfListener);
+        dopplerPitch = (SPEED_OF_SOUND - speedRelativeToListener) / SPEED_OF_SOUND;
+        PoliceAlarm.setPitch(dopplerPitch);
     }
 
     private void TravelToNextPoint()
